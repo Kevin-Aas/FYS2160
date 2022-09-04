87a01946ac38541d039aab4d9cec37c9574102fc
Kevin <kevinalexander@live.no>
2022-09-04 22:39:09 +0200

    ji

---
+++ oppg3-uke34.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+t = []
+T_kald = []
+T_varm = []
+
+with (open("metalblocks_lecture.tsv") as infile):
+    for i in infile:
+        t.append(float(i.split()[0]))
+        T_kald.append(float(i.split()[1]))
+        T_varm.append(float(i.split()[2]))
+        
+plt.plot(t, T_kald, label="T_kald")
+plt.plot(t, T_varm, label="T_varm")
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.25)
+plt.xlabel("Time (s)")
+plt.ylabel("Temperature (°C)")
+plt.legend()
+plt.show()
+
